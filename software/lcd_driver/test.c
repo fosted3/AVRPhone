9a3973991dfcff2f1d67a200cbf7a54f2d2f3e13
four0four <shmeeeh@gmail.com>
2014-03-29 02:01:44 -0400

    added lcd test files

---
+++ software/lcd_driver/test.c
@@ -0,0 +1,30 @@
+#include "pcd8544.h"
+
+// set up the SPI hardware, then initialize the pcd8544 and clear the display
+void lcdInit() {
+  LCD_DDR |= LCD_MASK;
+  LCD_SPI.CTRL = 0x52;
+
+  lcdReset();
+
+  lcdWrite(CMD_FUNC | FUNC_EXT, LCD_CMD);
+  lcdWrite(CMD_VOP | 0x31, LCD_CMD);
+  lcdWrite(CMD_TEMP, LCD_CMD); 
+  lcdWrite(CMD_BIAS | 0x04, LCD_CMD);
+  lcdWrite(CMD_FUNC, LCD_CMD);
+  lcdWrite(CMD_DISP | DISP_NORM, LCD_CMD);
+
+  lcdClear();
+}
+
+int main() {
+  lcdInit();
+  
+  for (int index = 0; index < 100; index++) {
+    lcdWrite(0x14, LCD_DATA);
+    lcdWrite(0x7F, LCD_DATA);
+    lcdWrite(0x14, LCD_DATA);
+    lcdWrite(0x7F, LCD_DATA);
+    lcdWrite(0x14, LCD_DATA);
+  } 
+}
